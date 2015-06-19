34aabcf97f6d373d4fe5cfd01b7d114fa6629e0c
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-19 14:30:25 -0500

    Displaying model data
    - ember-data 1.13.2 doesn't work with the TodoMVC tutorial. Must revisit
    - this change uses fixture data from the model to populate the initial todo list

--- js/router.js
+++ js/router.js
@@ -3,3 +3,21 @@
 Todos.Router.map(function() {
     this.resource('todos', { path: '/' });
 });
+
+// TodosRoute was provided by Ember.js
+// Because we hadn't implemented this class before, Ember.js provided a Route
+// for us with the default behavior of rendering a matching template named todos
+
+// Ember.js naming conventions #=> http://guides.emberjs.com/v1.10.0/concepts/naming-conventions/
+// for a route defined like: this.route('favorites');
+// Ember.js will look for App.FavoritesRoute, App.FavoritesController, and a
+// 'favorites' template
+
+// The below route deviates from default behavior because we need to load a
+// specific set of models
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo'); // returns all existing todos
+  }
+});
