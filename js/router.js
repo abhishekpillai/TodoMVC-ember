eb73384e70ce6f1ece8039a257bdd49f5b86f96f
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-19 12:53:29 -0500

    Create first route and template
    - Ember 1.13.0 and 2.0.0 do not work with the TodoMVC tutorial. Must revisit this to correct the error. For now, chugging along

---
+++ js/router.js
@@ -0,0 +1,5 @@
+Todos.Router.map(function() {
+    this.resource('todos', { path: '/' });
+});
+// This will tell Ember.js to detect when the application's
+// URL matches '/' and to render the todos template
