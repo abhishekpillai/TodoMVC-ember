1a06c16c59b45308753f9074e1fa025fcfd95b49
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-20 11:31:41 -0500

    Adding first child route
    - Move the entire <ul> of todos into a new template named todos/index
    - Add TodosIndexRoute to return the model data for the route

--- js/router.js
+++ js/router.js
@@ -1,17 +1,19 @@
 // This will tell Ember.js to detect when the application's
 // URL matches '/' and to render the todos template
 Todos.Router.map(function() {
-    this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function () {
+    // additional child routes will go here later
+  });
 });
 
 // TodosRoute was provided by Ember.js
 // Because we hadn't implemented this class before, Ember.js provided a Route
 // for us with the default behavior of rendering a matching template named todos
 
-// Ember.js naming conventions #=> http://guides.emberjs.com/v1.10.0/concepts/naming-conventions/
-// for a route defined like: this.route('favorites');
-// Ember.js will look for App.FavoritesRoute, App.FavoritesController, and a
-// 'favorites' template
+// Ember.js naming conventions #=>
+// http://guides.emberjs.com/v1.10.0/concepts/naming-conventions/ for a route
+// defined like: this.route('favorites'); Ember.js will look for
+// App.FavoritesRoute, App.FavoritesController, and a 'favorites' template
 
 // The below route deviates from default behavior because we need to load a
 // specific set of models
@@ -21,3 +23,14 @@ Todos.TodosRoute = Ember.Route.extend({
     return this.store.find('todo'); // returns all existing todos
   }
 });
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+// When '/' is loaded, Ember.js calls the todos route and render the todos
+// template. It also calls the todos.index and fills the {{outlet}} in the
+// todos template withe todos/index template value. The model data for this
+// template is the result of the model method of the TodosIndexRoute
