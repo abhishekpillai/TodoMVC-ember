c8a9f7453c35c007ef975599e77405c1bc493f8a
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-20 12:06:47 -0500

    Clear completed button now works
    - clearCompleted action is triggered. This action deletes completed todos. This triggers the completed fn to be executed and thus the hasCompleted fn.

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -16,17 +16,34 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
+    },
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+
+     // the invoke method which is part of the EmberArray API. The invoke will
+     // execute a method on each object in the Array if the method exists on
+     // that object
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
   },
 
+  // If the isCompleted value of any todo changes, this property will be recomputed
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
-  // If the isCompleted value of any todo changes, this property will be recomputed
 
+  // if remaining changes, the inflection fn is executed
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'todo' : 'todos';
-  }.property('remaining')
-  // if remaining changes, the inflection fn is executed
+  }.property('remaining'),
+
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 });
