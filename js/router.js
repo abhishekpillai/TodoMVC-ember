bfaefc1e425e393fcba4c9e90595accb5e079411
Abhi Pillai <abhi.pillai1@gmail.com>
2015-06-20 11:51:59 -0500

    Add ability to show only incomplete todos
    - Add a 'active' child route
    - Have the child route return only incomplete todos
    - Configure child route to render todos/index controller, instead of default behavior

--- js/router.js
+++ js/router.js
@@ -3,6 +3,7 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function () {
     // additional child routes will go here later
+    this.route('active');
   });
 });
 
@@ -17,16 +18,9 @@ Todos.Router.map(function() {
 
 // The below route deviates from default behavior because we need to load a
 // specific set of models
-
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo'); // returns all existing todos
-  }
-});
-
-Todos.TodosIndexRoute = Ember.Route.extend({
-  model: function() {
-    return this.modelFor('todos');
   }
 });
 
@@ -34,3 +28,24 @@ Todos.TodosIndexRoute = Ember.Route.extend({
 // template. It also calls the todos.index and fills the {{outlet}} in the
 // todos template withe todos/index template value. The model data for this
 // template is the result of the model method of the TodosIndexRoute
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+});
+
+// Normally transitioning into a new route changes the template rendered into
+// the parent {{outlet}}, but in this case we'd like to reuse the existing
+// todos/index template. We can accomplish this by implementing the
+// renderTemplate method and calling render ourselves with the specific
+// template and controller options.
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
